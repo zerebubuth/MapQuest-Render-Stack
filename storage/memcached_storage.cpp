398864d7188bdca12b31608204c898f21581cdcf
Jochen Topf <jochen@topf.org>
2012-09-26 18:35:56 +0200

    Improved error checking and logging

--- storage/memcached_storage.cpp
+++ storage/memcached_storage.cpp
@@ -26,10 +26,12 @@
 #include <sstream>
 #include <cassert>
 #include <boost/make_shared.hpp>
+
 #include <libmemcached/memcached.h>
 
-#include "memcached_storage.hpp"
+#include "logging/logger.hpp"
 #include "null_handle.hpp"
+#include "memcached_storage.hpp"
 
 using std::string;
 using std::time_t;
@@ -44,11 +46,12 @@ namespace
 tile_storage * create_memcached_storage(boost::property_tree::ptree const& pt,
                                         boost::optional<zmq::context_t &> ctx)
 {
-   std::string options = pt.get<std::string>("options", "");
+   std::string options = pt.get<std::string>("options", "--SERVER=localhost");
    int expire_in_minutes = pt.get<int>("expire", 0);
 
    /* Memcached interprets an expire time of more than 30 days as an absolute
-    * time stamp, we don't want this so we set this to 0 to mean "no expiry". */
+    * time stamp, we don't want this so we set this to 0 to mean "no expiry".
+    * In this case tiles are only expired when there is not enough memory (LRU). */
    if (expire_in_minutes < 0 || expire_in_minutes > 30 * 24 * 60) {
       expire_in_minutes = 0;
    }
@@ -60,33 +63,6 @@ const bool registered = register_tile_storage("memcached", create_memcached_stor
 
 } // anonymous namespace
 
-memcached_storage::handle::handle(const std::pair<metatile_reader::iterator_type, metatile_reader::iterator_type>& p)
-   : tile_data(p.first, p.second)
-{
-}
-
-memcached_storage::handle::~handle()
-{
-}
-
-bool
-memcached_storage::handle::exists() const
-{
-   return true;
-}
-
-std::time_t
-memcached_storage::handle::last_modified() const
-{
-   return 0;
-}
-
-bool
-memcached_storage::handle::expired() const
-{
-   return false;
-}
-
 bool
 memcached_storage::handle::data(string& output) const
 {
@@ -94,25 +70,33 @@ memcached_storage::handle::data(string& output) const
    return true;
 }
 
-memcached_storage::memcached_storage(const std::string& options, int expire_in_minutes) : expire_in_seconds(expire_in_minutes * 60)
+memcached_storage::memcached_storage(const std::string& options, int expire_in_minutes) :
+   expire_in_seconds(expire_in_minutes * 60),
+   memcache(memcached(options.c_str(), options.size()))
 {
-   memcache = memcached(options.c_str(), options.size());
+   LOG_INFO(boost::format("Initializing memcached storage with expire=[%1% minutes], options=[%2%].") % expire_in_minutes % options);
+   if (!memcache) {
+      LOG_ERROR("Can not initialize memcached storage. (This might be due to a missing or wrong 'options' setting in the config file.)");
+      throw std::runtime_error("Failed to create memcached storage.");
+   }
 }
 
 memcached_storage::~memcached_storage()
 {
-   memcached_free(memcache);
+   if (memcache) {
+      memcached_free(memcache);
+   }
 }
 
 shared_ptr<tile_storage::handle>
 memcached_storage::get(const tile_protocol &tile) const
 {
-   std::cerr << "memcached_storage::get style=" << tile.style << " z=" << tile.z << " x=" << tile.x << " y=" << tile.y << "\n";
+   LOG_DEBUG(boost::format("memcached_storage::get style=%1% z=%2% x=%3% y=%4%") % tile.style % tile.z % tile.x % tile.y);
 
    std::string data;
    if (!get_meta(tile, data))
    {
-      std::cerr << "  not found\n";
+      LOG_DEBUG("memcached_storage::get tile not found");
       return shared_ptr<tile_storage::handle>(new null_handle());
    }
 
@@ -121,9 +105,11 @@ memcached_storage::get(const tile_protocol &tile) const
    if (tile_data.first == tile_data.second)
    {
       std::cerr << "  metatile format corrupt\n";
+      LOG_ERROR(boost::format("Metatile corrupt (style=%1% z=%2% x=%3% y=%4%).") % tile.style % tile.z % tile.x % tile.y);
       return shared_ptr<tile_storage::handle>(new null_handle());
    }
-   std::cerr << "  ok\n";
+
+   LOG_DEBUG("memcached_storage::get tile found");
    return boost::make_shared<handle>(tile_data);
 }
 
@@ -142,7 +128,7 @@ std::string memcached_storage::key_string(const tile_protocol &tile) const
 
 bool memcached_storage::get_meta(const tile_protocol &tile, std::string &data) const
 {
-   std::cerr << "memcached_storage::get_meta style=" << tile.style << " z=" << tile.z << " x=" << tile.x << " y=" << tile.y << "\n";
+   LOG_DEBUG(boost::format("memcached_storage::get_meta style=%1% z=%2% x=%3% y=%4%") % tile.style % tile.z % tile.x % tile.y);
    std::string key = key_string(tile);
    size_t value_length;
    uint32_t flags;
@@ -159,11 +145,12 @@ bool memcached_storage::get_meta(const tile_protocol &tile, std::string &data) c
 
 bool memcached_storage::put_meta(const tile_protocol &tile, const std::string &buf) const
 {
-   std::cerr << "memcached_storage::put_meta style=" << tile.style << " z=" << tile.z << " x=" << tile.x << " y=" << tile.y << "\n";
+   LOG_DEBUG(boost::format("memcached_storage::put_meta style=%1% z=%2% x=%3% y=%4%") % tile.style % tile.z % tile.x % tile.y);
    std::string key = key_string(tile);
    memcached_return_t rc = memcached_set(memcache, key.c_str(), key.size(), buf.c_str(), buf.size(), expire_in_seconds, (uint32_t)0);
    if (rc != MEMCACHED_SUCCESS)
    {
+      LOG_ERROR(boost::format("Can not store metatile in memcached (%1%).") % key);
       return false;
    }
    return true;
@@ -174,7 +161,7 @@ bool memcached_storage::put_meta(const tile_protocol &tile, const std::string &b
  */
 bool memcached_storage::expire(const tile_protocol &tile) const
 {
-   std::cerr << "memcached_storage::expire style=" << tile.style << " z=" << tile.z << " x=" << tile.x << " y=" << tile.y << "\n";
+   LOG_DEBUG(boost::format("memcached_storage::expire style=%1% z=%2% x=%3% y=%4%") % tile.style % tile.z % tile.x % tile.y);
    std::string key = key_string(tile);
    memcached_return_t rc = memcached_delete(memcache, key.c_str(), key.size(), 0);
    if (rc != MEMCACHED_SUCCESS)
